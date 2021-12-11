0fce1cbad829042dec3c697dfabb8f382d036924
Niels Lindgren <niels@uber.com>
2021-12-11 12:41:16 -0800

    Day 7 Separate Solution File

--- go/day07/day07.go
+++ go/day07/day07.go
@@ -1,49 +1,11 @@
 package day07
 
 import (
-	"io"
-	"io/ioutil"
 	"math"
 	"sort"
 	"strconv"
 	"strings"
 )
-
-type Solution struct {
-	nums []int
-}
-
-func (s *Solution) Name() string {
-	return "The Treachery of Whales"
-}
-
-func (s *Solution) Load(r io.Reader) error {
-	b, err := ioutil.ReadAll(r)
-	if err != nil {
-		return err
-	}
-
-	nums, err := ParseNums(string(b))
-	if err != nil {
-		return err
-	}
-
-	s.nums = nums
-
-	return nil
-}
-
-func (s *Solution) PartOne() (string, error) {
-	mid := MidPoint(s.nums)
-
-	return strconv.Itoa(int(SillyLineSearch(L1(s.nums), mid, 256))), nil
-}
-
-func (s *Solution) PartTwo() (string, error) {
-	mid := MidPoint(s.nums)
-
-	return strconv.Itoa(int(SillyLineSearch(L2(s.nums), mid, 256))), nil
-}
 
 type CostFunction func(int) float64
 
