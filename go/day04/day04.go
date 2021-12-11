30e51a481a09c7722c7bc1564d43311d23d51f80
Niels Lindgren <niels@uber.com>
2021-12-11 12:38:53 -0800

    Day 4 Separate Solution File

--- go/day04/day04.go
+++ go/day04/day04.go
@@ -8,45 +8,6 @@ import (
 	"strings"
 )
 
-type Solution struct {
-	rand   *RandomNumbers
-	boards []Board
-}
-
-func (s *Solution) Name() string {
-	return "Giant Squid"
-}
-
-func (s *Solution) Load(r io.Reader) error {
-	rand, boards, err := Parse(r)
-	if err != nil {
-		return err
-	}
-
-	s.rand = rand
-	s.boards = boards
-
-	return nil
-}
-
-func (s *Solution) PartOne() (string, error) {
-	winner, err := WinFirst(s.rand, s.boards)
-	if err != nil {
-		return "", err
-	}
-
-	return strconv.Itoa(s.rand.Score(winner)), nil
-}
-
-func (s *Solution) PartTwo() (string, error) {
-	winner, err := WinLast(s.rand, s.boards)
-	if err != nil {
-		return "", err
-	}
-
-	return strconv.Itoa(s.rand.Score(winner)), nil
-}
-
 func ParseRand(line string) (*RandomNumbers, error) {
 	strs := strings.Split(line, ",")
 	nums := make([]int, len(strs))
