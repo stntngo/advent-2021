633d7545746c86bc7d10fffc11798373a634e1d0
Niels Lindgren <niels@uber.com>
2021-12-11 12:44:02 -0800

    Day 10 Separate Solution File

--- go/day10/day10.go
+++ go/day10/day10.go
@@ -6,38 +6,8 @@ import (
 	"fmt"
 	"io"
 	"sort"
-	"strconv"
 	"strings"
 )
-
-type Solution struct {
-	lines []string
-}
-
-func (s *Solution) Name() string {
-	return "Syntax Scoring"
-}
-
-func (s *Solution) Load(r io.Reader) error {
-	lines, err := Lines(r)
-	if err != nil {
-		return err
-	}
-
-	s.lines = lines
-
-	return nil
-}
-
-func (s *Solution) PartOne() (string, error) {
-	corruption, _ := Score(s.lines)
-	return strconv.Itoa(corruption), nil
-}
-
-func (s *Solution) PartTwo() (string, error) {
-	_, autocomplete := Score(s.lines)
-	return strconv.Itoa(autocomplete), nil
-}
 
 type ErrCorruptedLine struct {
 	expected, got string
