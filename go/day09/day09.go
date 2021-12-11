e86c67a75ca9b303e1dcf66a28b9f85462bb4f0e
Niels Lindgren <niels@uber.com>
2021-12-11 12:43:16 -0800

    Day 9 Separate Solution File

--- go/day09/day09.go
+++ go/day09/day09.go
@@ -8,33 +8,6 @@ import (
 	"strings"
 )
 
-type Solution struct {
-	hm HeightMap
-}
-
-func (s *Solution) Name() string {
-	return "Smoke Basin"
-}
-
-func (s *Solution) PartOne() (string, error) {
-	return strconv.Itoa(s.hm.RiskLevel()), nil
-}
-
-func (s *Solution) PartTwo() (string, error) {
-	return strconv.Itoa(s.hm.BasinFactor()), nil
-}
-
-func (s *Solution) Load(r io.Reader) error {
-	hm, err := Parse(r)
-	if err != nil {
-		return err
-	}
-
-	s.hm = hm
-
-	return nil
-}
-
 type HeightNode struct {
 	Coordinate
 	Value int
