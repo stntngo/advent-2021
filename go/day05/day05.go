4bfc0ab09981b35c31e852d45d8707a69975bb70
Niels Lindgren <niels@uber.com>
2021-12-11 12:39:33 -0800

    Day 5 Separate Solution File

--- go/day05/day05.go
+++ go/day05/day05.go
@@ -8,41 +8,6 @@ import (
 	"strings"
 )
 
-type Solution struct {
-	lines []Line
-}
-
-func (s *Solution) Name() string {
-	return "Hydrothermal Venture"
-}
-
-func (s *Solution) Load(r io.Reader) error {
-	lines, err := Parse(r)
-	if err != nil {
-		return err
-	}
-
-	s.lines = lines
-
-	return nil
-}
-
-func (s *Solution) PartOne() (string, error) {
-	noDiagonals := make([]Line, 0, len(s.lines))
-
-	for _, line := range s.lines {
-		if line.LineType() != Diagonal {
-			noDiagonals = append(noDiagonals, line)
-		}
-	}
-
-	return strconv.Itoa(CountHotSpots(noDiagonals)), nil
-}
-
-func (s *Solution) PartTwo() (string, error) {
-	return strconv.Itoa(CountHotSpots(s.lines)), nil
-}
-
 type LineType int
 
 const (
