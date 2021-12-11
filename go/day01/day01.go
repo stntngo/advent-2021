66c95644fc2d11152df8356fafdec02991cf21ac
Niels Lindgren <niels@uber.com>
2021-12-11 12:35:48 -0800

    Day 1 Separate Solution File

--- go/day01/day01.go
+++ go/day01/day01.go
@@ -6,33 +6,6 @@ import (
 	"strconv"
 )
 
-type Solution struct {
-	reading SonarReading
-}
-
-func (s *Solution) Name() string {
-	return "Sonar Sweep"
-}
-
-func (s *Solution) Load(r io.Reader) error {
-	reading, err := ParseSonarReading(r)
-	if err != nil {
-		return err
-	}
-
-	s.reading = reading
-
-	return nil
-}
-
-func (s *Solution) PartOne() (string, error) {
-	return strconv.Itoa(s.reading.DepthIncrease()), nil
-}
-
-func (s *Solution) PartTwo() (string, error) {
-	return strconv.Itoa(s.reading.SlidingWindow().DepthIncrease()), nil
-}
-
 type SonarReading []int
 
 func (r SonarReading) DepthIncrease() int {
