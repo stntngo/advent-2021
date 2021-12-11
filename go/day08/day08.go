f1fee7c36cb23807f7e675aba5284a23f9ce886f
Niels Lindgren <niels@uber.com>
2021-12-11 12:42:01 -0800

    Day 8 Separate Solutions File

--- go/day08/day08.go
+++ go/day08/day08.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"io"
 	"math"
-	"strconv"
 	"strings"
 )
 
@@ -31,33 +30,6 @@ func init() {
 		copy(b, a)
 		_COMBINATIONS = append(_COMBINATIONS, b)
 	})
-}
-
-type Solution struct {
-	signals []Signal
-}
-
-func (s *Solution) Name() string {
-	return "Seven Segment Search"
-}
-
-func (s *Solution) Load(r io.Reader) error {
-	signals, err := Parse(r)
-	if err != nil {
-		return err
-	}
-
-	s.signals = signals
-
-	return nil
-}
-
-func (s *Solution) PartOne() (string, error) {
-	return strconv.Itoa(EasyDigitCount(s.signals)), nil
-}
-
-func (s *Solution) PartTwo() (string, error) {
-	return strconv.Itoa(SignalOutputSum(s.signals)), nil
 }
 
 // Permutations calls f with each permutation of a.
